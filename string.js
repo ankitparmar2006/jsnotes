f7eb290fb7b4029f90dc18b28afa9124949c4ba9
ankitparmar2006 <ankitparmar30202@gmail.com>
2024-11-27 22:42:10 +0530

    update

---
+++ string.js
@@ -0,0 +1,79 @@
+
+ 
+
+// // 27 nov
+// // template lateral =          ` `
+
+// const letsgo =()=>{
+
+// para =`"this "is" string"`;   
+// console.log(para)
+// }
+// letsgo ();
+
+// //---------------------------------------------------------------------
+// benefits of template laterals
+
+// `"this "ankit" string"`
+
+// `this is 
+// ankit`
+
+// `value of ${s+2} s is `
+
+//---------------------------------------------------------------------------
+
+// const letsgo =()=>{
+//     let s=56;
+//     let s2=5;
+//     para =`the sum of ${s}+ ${s2}+ ${s+s2} `;   
+//     console.log(para)
+//     }
+//     letsgo ();
+    
+
+    //---------------------------------------------------------------------------
+
+ //------------------imidetly envoked function (iife)------------
+    
+
+//  (function(){
+//     console.log("hello world")
+//  }())
+
+
+//  thin tarha ke function hote hello
+
+//  arrow and function () and last this 
+
+//  (function(){
+//     console.log("hello world")
+//  }())
+
+
+    //---------------------------------------------------------------------------
+
+
+     //------------------parameter function------------
+
+
+//      let go = (a,b)=>{
+
+// console.log (2+7)
+
+//      }
+// go (2,7)
+
+//---------------------------------------------------
+
+// let go = (a,b)=>{
+
+//   return a+b;               //agr ans ka he kuch krna h tab return use krte h
+
+//  }
+//    let ans = go(2,7)
+//   console.log(ans*2)
+//   console.log(ans/2)
+//   console.log(ans-2)
+//   console.log(ans+2)
+
