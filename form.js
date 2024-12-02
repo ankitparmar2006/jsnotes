cf4ab12cc350e04727fd1695192989ad59d17ac1
ankitparmar2006 <ankitparmar30202@gmail.com>
2024-12-02 23:04:36 +0530

    update

--- form.js
+++ form.js
@@ -1,102 +1,81 @@
-let sub =()=>{
+let Sub=()=>{
 
-    let name=document.querySelector("#name").value;    
-    
-    let num=document.querySelector("#num").value;
-    
+    let name=document.querySelector("#name").value;
+    let number=document.querySelector("#num").value;
     let email=document.querySelector("#email").value;
-    
     let pass=document.querySelector("#pass").value;
-    
     let cpass=document.querySelector("#cpass").value;
-    
-    
+ 
     let errorname=document.querySelector("#errorname");
-    
     let errornum=document.querySelector("#errornum");
-    
     let erroremail=document.querySelector("#erroremail");
-    
     let errorpass=document.querySelector("#errorpass");
-    
     let errorcpass=document.querySelector("#errorcpass");
-    
-    
-    
-    if (name===""){
-    
-        errorname.innerHTML="please enter your name";
-        errorname.style.color="red";
-        errorname.style.fontSize="20px";
-    return false;
-    
-    }
-    
-    else if (num===""){
-    
-        errorname.innerHTML="please enter your number";
-        errorname.style.color="red";
-        errorname.style.fontSize="20px";
-        return false;
-     
-    }
-    
-    
-    else if (isNaN(num)){
-    
-        errorname.innerHTML="please enter only number";
-        errorname.style.color="red";
-        errorname.style.fontSize="20px";
-        return false;
-     
-    }
-    
-    else if (num.length !== 10){
-    
-        errorname.innerHTML="please enter vaild number";
-        errorname.style.color="red";
-        errorname.style.fontSize="20px";
-        return false;
-     
-    }
-    
-    else if (!(email.includes("@") && email.includes (".com"))){
-    
-        errorname.innerHTML="please enter vaild email";
-        errorname.style.color="red";
-        errorname.style.fontSize="20px";
-        return false;
-     
-    }
-    
-    
-    else if (pass=""){
-    
-        errorname.innerHTML="please enter psswd";
-        errorname.style.color="red";
-        errorname.style.fontSize="20px";
-        return false;
-     
-    }
-    
-    else if (! (errorpass.lenght !== 8)) {
-    
-    
-    errorname.innerHTML="please enter 8 digit";
-        errorname.style.color="red";
-        errorname.style.fontSize="20px";
-        return false;
-    }
-    
-    
-    else if (!(cpass = pass)){
-    
-        errorname.innerHTML=" psswd not matiching";
-        errorname.style.color="red";
-        errorname.style.fontSize="20px";
-        return false;
-     
-    }
-    
-    
-    }
+ 
+        if(name===""){
+         document.querySelector("#name").placeholder="Enter Name"
+         // name.placeholder="Please enter your name";
+         errorname.style.color="red";
+         errorname.style.fontSize="30px";
+         return false;
+        }
+ 
+        else if(number===""){
+         errornum.textContent="Please enter your number";
+         errornum.style.color="red";
+         errornum.style.fontSize="30px";
+         return false;
+        }
+ 
+        else if(isNaN(number)){
+       
+         errornum.innerHTML="Please enter number only";
+         return false;
+ 
+        }
+ 
+        else if(number.length!==10){
+         errornum.innerHTML="Please enter valid number 10";
+         return false 
+ 
+        }
+ 
+        
+        else if(!(email.includes("@") && email.includes(".com"))){
+         erroremail.textContent="Please enter valid email";
+         erroremail.style.color="red";
+         erroremail.style.fontSize="30px"; 
+         return false;
+        }
+ 
+ 
+        else if(!
+         (
+             pass.match(/[1234567890]/) 
+              && 
+             pass.match(/[!@#$%^&*()]/) 
+              && 
+             pass.match(/[a-z]/) 
+              && 
+             pass.match(/[A-Z]/)
+         )
+     ){
+         errorpass.innerHTML = "Password contain atleast 1 lower, upper ,special and number";
+         document.querySelector('#errorpass').style.color = "red";
+         let passinp=document.querySelector("#pass")
+         passinp.style.borderColor = "red"
+         passinp.style.outlineColor = "red"
+         return false;
+     }
+ 
+     else if(pass !== cpass){
+         errorcpass.innerHTML = "Password and confirm password should be same";
+         document.querySelector('#pass').value = "";
+         document.querySelector('#cpass').value = "";
+         document.querySelector('#pass').focus();          
+         return false;
+     }
+ 
+ }
+
+ 
