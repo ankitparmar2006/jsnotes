310f818166b1b29dc7b33435816047fff4c457ec
ankitparmar2006 <ankitparmar30202@gmail.com>
2024-12-03 23:42:37 +0530

    new file

--- purejs.js
+++ purejs.js
@@ -50,7 +50,7 @@ let btn4=document.querySelector("#btn4");
 
 btn4.addEventListener("dblclick", function(){
 
-    btn4.innerHTML="hello user"
+    btn4.innerHTML="hamare yaha essa he hota hai"
 
 }
 )
@@ -62,12 +62,13 @@ let btn5 = document.querySelector("#btn5");
 
 btn5.addEventListener("mouseenter", function (){
 
+
+
 btn5.innerHTML="toh kese hai aap log"
 
 btn5.addEventListener("mouseleave", function (){
 
     btn5.innerHTML="thik hai "
-    
     
     
     
@@ -84,9 +85,9 @@ btn5.addEventListener("mouseleave", function (){
 
  let inpp=document.querySelector("#inp");
 
- inpp.addEventListener("keydown", function (){
+ inpp.addEventListener("keydown", function (e){
 
-    console.log(inpp.value)
+    console.log(e.key)
 
  }
 )
@@ -104,3 +105,42 @@ inp.addEventListener("input", function (){
 
 }
 )
+
+//====================================================================================================
+
+let str = ()=> {
+
+let im =document.querySelector("#im");
+
+im.src='Screenshot 2024-11-15 213209.png';
+
+}
+
+
+//--------------------------------------------------------------------------------------------------------
+
+// let str2 = ()=> {
+
+//     let im =document.querySelector("#im");
+    
+//     im.src='Screenshot 2024-11-15 213326.png';
+    
+//     }
+
+
+str =()=>{
+let imgg = document.querySelector("#img")
+
+imgg.src="img2.png";
+
+}
+
+
+
+str2 =()=>{
+    let imggg = document.querySelector("#img")
+    
+    imggg.src='img1.png';
+    
+    }
+//====================================================================================================
